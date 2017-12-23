f66137046e13a50bd6785a0558ad9cc9d7880209
DeAlexPesh <DeAlexWin@gmail.com>
2017-12-24 04:50:50 +0500

    Update colored.js

--- js/colored.js
+++ js/colored.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   var c = ['rgba(255,0,0,0.2)','rgba(0,255,0,0.2)','rgba(0,0,255,0.2)'];
   var i = 0;  
   setInterval(function() {
-    $('#colored').animate({ backgroundColor: c[i] }, 4000);
+    $('#colored').stop().animate({ backgroundColor: c[i] }, 4000);
     i = i < 2 ? i + 1 : 0;
-    alert($('#colored').css( "background-color" ));
   }, 5000);  
 });
