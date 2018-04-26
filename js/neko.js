7f1701b3f4987a9c64acde9ef53379f7e5436fe4
DeAlexPesh <DeAlexWin@gmail.com>
2018-04-27 00:09:01 +0500

    Update neko.js

--- js/neko.js
+++ js/neko.js
@@ -1,25 +1,35 @@
 $(document).ready(function() {
+  
   function switchImg(classImg) {
     return new Promise(function(resolve, reject) {
       var $oldImg = $('#neko-wrapper').find('.neko:first')
       $('#neko-wrapper').prepend( $('<div class="neko ' + classImg + '"></div>').css({ opacity: 0 }) );
       $('#neko-wrapper').find('.neko:first').animate({ opacity: 1 }, 500, function() { 
-        //$oldImg.remove(); 
+        $oldImg.remove(); 
         resolve(""); 
       });
     });
   };
-  
-  setInterval(function() {
-    $('#neko-wrapper').animate({ left: 0 }, 2000, function() {
-      setTimeout(function() {
-        switchImg('p01').then(function() {
-          $('#neko-wrapper').animate({ left: -301 }, 1000, function() {
-            switchImg('p00');
+
+  function preImg() {
+    return new Promise(function(resolve, reject) {
+      $('#neko-wrapper').children().not('.neko:first').remove();
+      resolve($slides);
+    }
+  });
+    
+  preImg().then(function() {
+    setInterval(function() {
+      $('#neko-wrapper').animate({ left: 0 }, 2000, function() {
+        setTimeout(function() {
+          switchImg('p01').then(function() {
+            $('#neko-wrapper').animate({ left: -301 }, 1000, function() {
+              switchImg('p00');
+            })
           })
-        })
-      }, 2000)
-    })
-  }, 6000);//Math.floor(Math.random() * (300000 - 6000)) + 6000);
-  
+        }, 2000)
+      })
+    }, 6000);//Math.floor(Math.random() * (300000 - 6000)) + 6000);
+  });
+    
 });
