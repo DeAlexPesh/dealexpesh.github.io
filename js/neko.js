e1d9ed1cf569ed6af80787222e0332ad8dbed71f
DeAlexPesh <DeAlexWin@gmail.com>
2018-04-26 15:50:33 +0500

    Create neko.js

---
+++ js/neko.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  function switchImg(classImg) {
+    return new Promise(function(resolve, reject) {
+      var $oldImg = $('#neko-wrapper').find('.neko:first')
+      $('#neko-wrapper').prepend( $('<div class="neko ' + classImg + '"></div>').css({ display: 0 }) );
+      $('#neko-wrapper').find('.neko:first').animate({ display: 1 }, 500, function() { $oldImg.remove(); resolve(""); });
+    });
+  };
+  setInterval(function() {
+    $('#neko-wrapper').animate({ left: 0 }, 2000, function() {
+      setTimeout(function() {
+        switchImg('p01').then(function() {
+          $('#neko-wrapper').animate({ left: -301 }, 1000, function() {
+            switchImg('p00');
+          })
+        })
+      }, 2000)
+    })
+  }, Math.floor(Math.random() * (300000 - 6000)) + 6000);
+});
