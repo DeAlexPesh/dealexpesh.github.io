d519ada1f24129791de5dd3d49c0c32d4771eb33
DeAlexPesh <DeAlexWin@gmail.com>
2018-04-26 23:29:12 +0500

    Update neko.js

--- js/neko.js
+++ js/neko.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     });
   };
   
-  var arrImg = ['p00.png', 'p01.png']
+  var arrImg = [p00.png p01.png]
   $(arrImg).preloadImage().then(function() {
     setInterval(function() {
       $('#neko-wrapper').animate({ left: 0 }, 2000, function() {
